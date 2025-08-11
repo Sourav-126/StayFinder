bfbc6d62ab0ceb35c67ea03a69b38291e42b9370
Sourav Arora <souravarora285@gmail.com>
2025-08-11 23:07:45 +0530

    Build bugs

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
       },
     });
     return NextResponse.json(user, { status: 200 });
-  } catch (error) {
+  } catch {
     return NextResponse.json("Failed", { status: 500 });
   }
 }
